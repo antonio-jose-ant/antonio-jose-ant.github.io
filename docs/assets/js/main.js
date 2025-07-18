0b0e0ca04f5b1ba858f228430463b20a7b2f8f4c
antonio-jose-ant <1623jsser@gmail.com>
2025-07-18 00:22:04 -0600

    contenido inicial del portafolio

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,10 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
-    CargaModuloHTML.cargaModulo('./pages/header.html');
+    CargaModuloHTML.cargaModulo('inicio');
 });
 const CargaModuloHTML = (() => {
+    const idDOMPermitidos = ['inicio', 'section', 'footer'];
+    const rutasPermitidas = {
+        'inicio': './pages/inicio.html',
+        'section': './pages/section.html',
+        'footer': './pages/footer.html',
+        'header': './pages/header.html'
+    }
     return {
-        cargaModulo: (modulo,idperm=false) => {
-            fetch(modulo)
+        cargaModulo: (modulo, idperm = false) => {
+            if (!idDOMPermitidos.includes(idperm)) {
+                console.error(`El ID '${idperm}' no está permitido.`);
+                return;
+            }
+            if (!rutasPermitidas[modulo]) {
+                console.error(`El módulo '${modulo}' no está permitido.`);
+                return;
+            }
+            fetch(rutasPermitidas[modulo])
                 .then(response => {
                     if (!response.ok) {
                         throw new Error(`Error HTTP: ${response.status}`);
@@ -12,7 +27,10 @@ const CargaModuloHTML = (() => {
                     return response.text();
                 })
                 .then(htmlString => {
-                    document.getElementById(idperm?idperm:'section').innerHTML = htmlString;
+                    document.getElementById(idperm ? idperm : 'section').innerHTML = htmlString;
+                    if(modulo==="inicio"){
+                        document.getElementById('I_AM').style.height = window.innerHeight + 'px';
+                    }
                 })
                 .catch(error => {
                     console.error('Error al cargar el módulo:', error);
